249a6c6ea2bea5052a527ae0a1d70da7cec1dd02
Davi Miranda <davigmirandapro@gmail.com>
2024-12-03 13:37:39 -0300

    projeto full stack

---
+++ src/main/java/oi/davimiranda/contactapi/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package oi.davimiranda.contactapi.repo;
+
+import oi.davimiranda.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
---
+++ src/test/java/oi/davimiranda/contactapi/ContactapiApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.davimiranda.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
